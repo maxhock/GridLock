936909f8d64c71cd857b482223a8465eb0d6bb34
bereba <beneharo.reveron-baecker@tum.de>
2026-01-30 03:06:49 +0100

    Draft for infdb-data connector

---
+++ infdb-data/main.py
@@ -0,0 +1,41 @@
+"""
+Main entry point for the infdb-data tool.
+Handles InfDB initialization, database connection, logging, and execution.
+"""
+
+# Import packages
+import os
+
+from infdb import InfDB
+from src import infdb_data
+
+
+def main():
+    """
+    Initializes InfDB handler, sets up logging, connects to the database,
+    and runs the functions. Handles exceptions and logs errors.
+    """
+
+    # Initialize InfDB handler
+    infdb = InfDB(tool_name="infdb-data")
+
+    # Start message
+    log = infdb.get_logger()
+    log.info(f"Starting {infdb.get_toolname()} tool")
+
+    plz = infdb.get_config_value([infdb.get_toolname(), "data", "plz"])
+
+    try:
+        # ===========================================================
+        # Start your added python code in folder "src"
+        # ===========================================================
+        log.info("Start pylovo grid import")
+        infdb_data.get_pylovo_grid(plz=plz)
+
+    except Exception as e:
+        log.error(f"Something went wrong: {str(e)}")
+        infdb.stop_logger()
+        raise e
+
+if __name__ == "__main__":
+    main()
---
+++ infdb-data/src/infdb_data.py
@@ -0,0 +1,24 @@
+import os
+import geopandas as gpd
+from sqlalchemy import create_engine
+
+def get_pylovo_grid(infdb, plz: int) -> gpd.GeoDataFrame:
+    """
+    Pylovo grid querying using InfDB client.
+
+    Retrieves pandapower grid json from the pylovo schema using
+    the InfDB database engine and loads it into a GeoDataFrame.
+
+    Args:
+        infdb: InfDB client instance with database connection.
+        plz: postcode to query the pylovo grid for.
+
+    Returns:
+        GeoDataFrame: Buildings with heat demand data and geometry.
+    """
+    engine = infdb.get_db_engine()
+    sql = f"SELECT grid FROM pylovo.grid_results WHERE plz={plz}"
+    gdf_buildings = gpd.read_postgis(sql, engine)
+    gdf_buildings.head()
+
+    return gdf_buildings
